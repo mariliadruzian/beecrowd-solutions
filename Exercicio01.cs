13449a857d2f2344fcee4e9c5032bcef52925ca0
Marília Druzian <mariliadruzian@gmail.com>
2024-03-18 00:01:58 -0300

    exercise solution

--- Exercicio01.cs
+++ Exercicio01.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 // Fazer um programa para ler as medidas da base e altura de um retângulo. Em seguida, mostrar o valor da área, perímetro e diagonal deste retângulo, com quatro casas decimais.
 
-class Exercicio05
+class Exercicio01
 {
     static void Main(string[] args)
     {
