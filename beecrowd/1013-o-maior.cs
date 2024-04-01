02ea326a0f9cfc08aa2633dc15df587bbe92d0b7
Marília Druzian <mariliadruzian@gmail.com>
2024-04-01 00:35:44 -0300

    o maior

--- beecrowd/1013-o-maior.cs
+++ beecrowd/1013-o-maior.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 // Bee1013 - O Maior: Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
 // Fórmula: MaiorAB = (a + b + abs(a - b))/2
