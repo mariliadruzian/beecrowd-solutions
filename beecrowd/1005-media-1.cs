80005298fb08a120f1dfc4243e1fb7844093331b
Marília Druzian <mariliadruzian@gmail.com>
2024-04-01 00:21:21 -0300

    média 1

--- beecrowd/1005-media-1.cs
+++ beecrowd/1005-media-1.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Globalization;
 
-// Bee1005 - Média 1: Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11).
+// Bee1005 - Média 1: Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (a soma dos pesos portanto é 11).
 // Entrada: O arquivo de entrada contém 2 valores com uma casa decimal cada um.
-// Saída: Imprima a mensagem "MEDIA" e a média do aluno, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double).
+// Saída: Imprima a mensagem "MEDIA = " e a média do aluno, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double).
 
 class Bee1005
 {
