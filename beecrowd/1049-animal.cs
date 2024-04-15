82f15ce447b856255cb69ba6726db1c474c2d46b
Marília Druzian <mariliadruzian@gmail.com>
2024-04-14 22:32:01 -0300

    animal

---
+++ beecrowd/1049-animal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Bee1049
+{
+    static void Main(string[] args)
+    {
+        string x = Console.ReadLine();
+        string y = Console.ReadLine();
+        string z = Console.ReadLine();
+
+        if (x == "vertebrado")
+        {
+            if (y == "ave")
+            {
+                if (z == "carnivoro")
+                {
+                    Console.WriteLine("aguia");
+                }
+                else
+                {
+                    Console.WriteLine("pomba");
+                }
+            }
+            else
+            {
+                if (z == "onivoro")
+                {
+                    Console.WriteLine("homem");
+                }
+                else
+                {
+                    Console.WriteLine("vaca");
+                }
+            }
+        }
+        else
+        {
+            if (y == "inseto")
+            {
+                if (z == "hematofago")
+                {
+                    Console.WriteLine("pulga");
+                }
+                else
+                {
+                    Console.WriteLine("lagarta");
+                }
+            }
+            else
+            {
+                if (z == "hematofago")
+                {
+                    Console.WriteLine("sanguessuga");
+                }
+                else
+                {
+                    Console.WriteLine("minhoca");
+                }
+            }
+        }
+    }
+}
