4b311b9a128eace2ae2cf3905054884b0d549913
Marília Druzian <mariliadruzian@gmail.com>
2024-04-15 22:25:39 -0300

    imposto de renda

---
+++ beecrowd/1051-imposto-de-renda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+class Bee1051
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        double salario, basedecalc, imposto;
+
+        // Entrada
+        salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        // Saída
+        if (salario <= 2000.0)
+        {
+            Console.WriteLine("Isento");
+        }
+        else if (salario > 2000.0 && salario <= 3000.0)
+        {
+            basedecalc = (salario - 2000.0);
+            imposto = basedecalc * 8 / 100;
+            Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        else if (salario > 3000.0 && salario <= 4500.0)
+        {
+            basedecalc = (salario - 3000.0);
+            imposto = (basedecalc * 18 / 100) + (1000.0 * 8 / 100);
+            Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        else
+        {
+            basedecalc = (salario - 4500.0);
+            imposto = (basedecalc * 28 / 100) + (1500.0 * 18 / 100) + (1000.0 * 8 / 100);
+            Console.WriteLine($"R$ {imposto.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
