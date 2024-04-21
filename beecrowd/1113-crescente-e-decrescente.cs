6eed4ee2e753dddd665d021ab879b682e28a808a
Marília Druzian <mariliadruzian@gmail.com>
2024-04-21 16:09:43 -0300

    crescente e decrescente

---
+++ beecrowd/1113-crescente-e-decrescente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Bee1113
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        string[] vet;
+        int x, y;
+
+        // Entrada
+        vet = Console.ReadLine().Split(' ');
+        x = int.Parse(vet[0]);
+        y = int.Parse(vet[1]);
+
+        // Estrutura repetitiva
+        while (x != y)
+        {
+            if (x > y)
+            {
+                Console.WriteLine("Decrescente");
+            }
+            else
+            {
+                Console.WriteLine("Crescente");
+            }
+            vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+        }
+    }
+}
