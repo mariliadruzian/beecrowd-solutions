dd1b2936d2588784ace0d07a719f8cd4aa008900
Marília Druzian <mariliadruzian@gmail.com>
2024-04-21 23:14:37 -0300

    validação de nota

---
+++ beecrowd/1117-validacao-de-nota.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+class Bee1117
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        double nota1, nota2, media;
+
+        // Nota 1
+        nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (nota1 < 0 || nota1 > 10)
+        {
+            Console.WriteLine("nota invalida");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        // Nota 2
+        nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (nota2 < 0 || nota2 > 10)
+        {
+            Console.WriteLine("nota invalida");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        // Cálculo e saída da média
+        media = (nota1 + nota2) / 2.0;
+        Console.WriteLine($"media = {media.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
