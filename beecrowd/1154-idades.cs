6d062c8365c7511d7b3cdde27c8b64d0855f7d0b
Marília Druzian <mariliadruzian@gmail.com>
2024-04-22 23:18:07 -0300

    idades

---
+++ beecrowd/1154-idades.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+class Bee1154
+{
+    static void Main(string[] args)
+    {
+        int idade, soma = 0, cont = 0;
+        double media;
+
+        idade = int.Parse(Console.ReadLine());
+
+        while (idade >= 0)
+        {
+            soma += idade;
+            cont++;
+            idade = int.Parse(Console.ReadLine());
+        }
+
+        media = (double) soma / cont;
+        Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
