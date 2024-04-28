f3dce9cd953b8770c46e6c90dbdb86ad4de85dca
Marília Druzian <mariliadruzian@gmail.com>
2024-04-28 09:42:57 -0300

    intervalo 2

---
+++ beecrowd/1072-intervalo-2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Bee1072
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int n, x, i;
+        int inTotal = 0;
+        int outTotal = 0;
+
+        // Entrada de 'n'
+        n = int.Parse(Console.ReadLine());
+
+        // Entrada de 'x'
+        for (i = 1; i <= n; i++)
+        {
+            x = int.Parse(Console.ReadLine());
+            if (x >= 10 && x <= 20)
+            {
+                inTotal++;
+            }
+            else
+            {
+                outTotal++;
+            }
+        }
+
+        // Saída
+        Console.WriteLine($"{inTotal} in");
+        Console.WriteLine($"{outTotal} out");
+    }
+}
