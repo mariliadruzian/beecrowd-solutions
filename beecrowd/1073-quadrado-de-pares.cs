97205ee3381cb6fff5b9d4b8cee50cea6aa5ea29
Marília Druzian <mariliadruzian@gmail.com>
2024-04-28 10:00:17 -0300

    quadrado de pares

---
+++ beecrowd/1073-quadrado-de-pares.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Bee1073
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int n, i, resultado;
+
+        // Entrada
+        n = int.Parse(Console.ReadLine());
+
+        // Validação da entrada (5 < N < 2000)
+        while (n <= 5 || n >= 2000)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        // Saída
+        for (i = 2; i <= n; i += 2)
+        {
+            resultado = i * i;
+            Console.WriteLine($"{i}^2 = {resultado}");
+        }
+    }
+}
