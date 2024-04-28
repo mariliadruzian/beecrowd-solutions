0634eb3f55bd1ceafa3e0bb89f16445204b1344f
Marília Druzian <mariliadruzian@gmail.com>
2024-04-27 23:01:24 -0300

    números ímpares

---
+++ beecrowd/1067-numeros-impares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Bee1067
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int x, i;
+
+        // Entrada
+        x = int.Parse(Console.ReadLine());
+
+        // Valida a regra de entrada (1 <= X <= 1000)
+        while (x < 1 || x > 1000)
+        {
+            x = int.Parse(Console.ReadLine());
+        }
+
+        // Saída
+        for (i = 1; i <= x; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
