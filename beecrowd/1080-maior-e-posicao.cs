efd502f2b1259239e53d625366be34f6034dcc9f
Marília Druzian <mariliadruzian@gmail.com>
2024-04-28 11:52:54 -0300

    maior e posição

---
+++ beecrowd/1080-maior-e-posicao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Bee1080
+{
+    static void Main(string[] args)
+    {
+        int i, x;
+        int posicao = 0;
+        int max = 0;
+
+        for (i = 1; i <= 100; i++)
+        {
+            x = int.Parse(Console.ReadLine());
+            if (x > max)
+            {
+                max = x;
+                posicao = i;
+            }
+        }
+
+        Console.WriteLine(max);
+        Console.WriteLine(posicao);
+    }
+}
