12ac6fcd9ccbe5b61a592ff7aacafc0650a24745
Marília Druzian <mariliadruzian@gmail.com>
2024-04-28 11:27:12 -0300

    resto 2

---
+++ beecrowd/1075-resto-2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Bee1075
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int n, i;
+
+        // Entrada
+        n = int.Parse(Console.ReadLine());
+        
+        // Saída
+        for (i = 1; i <= 10000; i++)
+        {
+            if (i % n == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
