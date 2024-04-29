300f2f6266bfd06fc31ba5a8fd13254aa401151c
Marília Druzian <mariliadruzian@gmail.com>
2024-04-28 22:37:57 -0300

    sequência IJ 2

---
+++ beecrowd/1096-sequencia-ij-2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Bee1096
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 9; i += 2)
+        {
+            Console.WriteLine($"I={i} J=7");
+            Console.WriteLine($"I={i} J=6");
+            Console.WriteLine($"I={i} J=5");
+        }
+    }
+}
