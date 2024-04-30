497e15e1845f9420343e44119f1917171717f1c6
Marília Druzian <mariliadruzian@gmail.com>
2024-04-29 22:31:13 -0300

    dividindo x por y

---
+++ beecrowd/1116-dividindo-x-por-y.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+class Bee1116
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int n, i, x, y;
+        string[] valores;
+        double resultado;
+
+        // Entrada
+        n = int.Parse(Console.ReadLine());
+
+        // Saída
+        for (i = 1; i <= n; i++)
+        {
+            valores = Console.ReadLine().Split(' ');
+            x = int.Parse(valores[0]);
+            y = int.Parse(valores[1]);
+
+            if (y == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                resultado = (double)x / y;
+                Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
