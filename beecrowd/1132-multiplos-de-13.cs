ee1b4d218acf36b34fc2f80b4f83f4ffbf799376
Marília Druzian <mariliadruzian@gmail.com>
2024-04-29 22:53:28 -0300

    múltiplos de 13

---
+++ beecrowd/1132-multiplos-de-13.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Bee1132
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int x, y, i;
+        int maior, menor;
+        int soma = 0;
+
+        // Entrada
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        // Ordena do menor para o maior
+        if (x > y)
+        {
+            maior = x;
+            menor = y;
+        }
+        else
+        {
+            maior = y;
+            menor = x;
+        }
+
+        // Saída
+        for (i = menor; i <= maior; i++)
+        {
+            if (i % 13 != 0)
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine(soma);
+    }
+}
