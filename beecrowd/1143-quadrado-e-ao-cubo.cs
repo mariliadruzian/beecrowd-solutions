372bdca3dc4fb0299d8e2d4172e136e0908eda11
Marília Druzian <mariliadruzian@gmail.com>
2024-05-01 00:24:12 -0300

    quadrado e ao cubo

---
+++ beecrowd/1143-quadrado-e-ao-cubo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Bee1143
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int n, i;
+        int valor1 = 0;
+        int valor2 = 0;
+        int valor3 = 0;
+
+        // Entrada
+        n = int.Parse(Console.ReadLine());
+
+        // Saída
+        for (i = 1; i <= n; i++)
+        {
+            valor1 = i;
+            valor2 = i * i;
+            valor3 = i * i * i;
+            Console.WriteLine($"{valor1} {valor2} {valor3}");
+        }
+    }
+}
