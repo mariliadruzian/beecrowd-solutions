2f81026380764620e4a7729ec25eb5d686521ca0
Marília Druzian <mariliadruzian@gmail.com>
2024-05-01 00:04:13 -0300

    PUM

---
+++ beecrowd/1142-pum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Bee1142
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int i, n;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+
+        // Entrada
+        n = int.Parse(Console.ReadLine());
+
+        while (n <= 0)
+        {
+            n = int.Parse(Console.ReadLine());
+        }
+
+        // Saída
+        for (i = 1; i <= n; i++)
+        {
+            if (i == 1)
+            {
+                x = 1;
+                y = 2;
+                z = 3;
+            }
+            else
+            {
+                x += 4;
+                y += 4;
+                z += 4;
+            }
+            Console.WriteLine($"{x} {y} {z} PUM");
+        }
+    }
+}
