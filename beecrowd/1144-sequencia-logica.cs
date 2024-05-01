8a13a1a55408ebe2433966783b8e4c5d744cd3ca
Marília Druzian <mariliadruzian@gmail.com>
2024-05-01 00:46:41 -0300

    sequência lógica

---
+++ beecrowd/1144-sequencia-logica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Bee1144
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int i, n;
+        int valor1;
+        int valor2;
+
+        // Entrada
+        n = int.Parse(Console.ReadLine());
+
+        // Saída
+        for (i = 1; i <= n; i++)
+        {
+            valor1 = i * i;
+            valor2 = i * i * i;
+
+            Console.WriteLine($"{i} {valor1} {valor2}");
+            Console.WriteLine($"{i} {valor1 + 1} {valor2 + 1}");
+        }
+    }
+}
