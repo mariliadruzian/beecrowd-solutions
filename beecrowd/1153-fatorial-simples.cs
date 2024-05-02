a60ffaabfa5f00bb81c5ab2180c6088aaa095608
Marília Druzian <mariliadruzian@gmail.com>
2024-05-01 22:58:11 -0300

    fatorial simples

---
+++ beecrowd/1153-fatorial-simples.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Bee1153
+{
+    static void Main(string[] args)
+    {
+        int n, i; 
+        int fatorial = 0;
+
+        n = int.Parse(Console.ReadLine());
+
+        for (i = 1; i < n; i++)
+        {
+            if (i == 1)
+            {
+                fatorial = n * (n - i);
+            }
+            else
+            {
+                fatorial = fatorial * (n - i);
+            }
+        }
+
+        Console.WriteLine(fatorial);
+    }
+}
