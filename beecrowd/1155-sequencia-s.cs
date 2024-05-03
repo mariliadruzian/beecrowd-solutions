da40cf57589ca4176277264718f1f1220a20cfd9
Marília Druzian <mariliadruzian@gmail.com>
2024-05-02 21:42:28 -0300

    sequência S

---
+++ beecrowd/1155-sequencia-s.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class Bee1155
+{
+    static void Main(string[] args)
+    {
+        double s = 1.0;
+
+        for (int i = 2; i <= 100; i++)
+        {
+            s += (double)1 / i;
+        }
+
+        Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
