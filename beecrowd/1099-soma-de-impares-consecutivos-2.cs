cc3edd096b92b67326e8485705d10ff36029620e
Marília Druzian <mariliadruzian@gmail.com>
2024-05-02 23:53:38 -0300

    soma de ímpares consecutivos 2

---
+++ beecrowd/1099-soma-de-impares-consecutivos-2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Bee1099
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            int maior;
+            int menor;
+            if (x > y)
+            {
+                maior = x;
+                menor = y;
+            }
+            else
+            {
+                maior = y;
+                menor = x;
+            }
+
+            int soma = 0;
+            for (int j = menor + 1; j < maior; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    soma += j;
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
