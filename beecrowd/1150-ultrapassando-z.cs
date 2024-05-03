c92e8910c9f02e15fecf3985591ec061a6bec4d2
Marília Druzian <mariliadruzian@gmail.com>
2024-05-02 23:14:31 -0300

    ultrapassando Z

---
+++ beecrowd/1150-ultrapassando-z.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+
+class Bee1150
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int z = int.Parse(Console.ReadLine());
+
+        while (z <= x)
+        {
+            z = int.Parse(Console.ReadLine());
+        }
+
+        int soma = 0;
+        int cont = 0;
+        for (int i = x; soma <= z; i++)
+        {
+            soma += i;
+            cont++;
+        }
+
+        Console.WriteLine(cont);
+    }
+}
