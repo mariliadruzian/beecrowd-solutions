5b096b71c206ec0dc1ae10704f7ab5edf23f112d
Marília Druzian <mariliadruzian@gmail.com>
2024-05-02 22:21:45 -0300

    sequência S 2

---
+++ beecrowd/1156-sequencia-s-2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Bee1156
+{
+    static void Main(string[] args)
+    {
+        double s = 1;
+        int x = 2;
+
+        for (int i = 3; i <= 39; i += 2)
+        {
+            s += (double)i / x;
+            x += x;
+        }
+
+        Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
