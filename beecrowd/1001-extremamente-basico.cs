8c9b17702c8238c76949debce9878a00ce7a5efe
Marília Druzian <mariliadruzian@gmail.com>
2024-05-05 19:24:10 -0300

    extremamente básico

---
+++ beecrowd/1001-extremamente-basico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Bee1001
+{
+    static void Main(string[] args)
+    {
+        int a, b, sum;
+
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        sum = a + b;
+
+        Console.WriteLine($"X = {sum}");
+    }
+}
