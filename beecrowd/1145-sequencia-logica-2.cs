93b481d90f74b121a06c641aab98eb4e7ffaaf89
Marília Druzian <mariliadruzian@gmail.com>
2024-05-05 19:06:11 -0300

    sequência lógica 2

---
+++ beecrowd/1145-sequencia-logica-2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Bee1145
+{
+    static void Main(string[] args)
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        int x = int.Parse(vet[0]);
+        int y = int.Parse(vet[1]);
+
+        int cont = 0;
+        for (int i = 1; i <= y; i++)
+        {
+            Console.Write($"{i}");
+            cont++;
+
+            if (cont == x)
+            {
+                Console.WriteLine();
+                cont = 0;
+            }
+            else
+            {
+                Console.Write(' ');
+            }
+        }
+    }
+}
