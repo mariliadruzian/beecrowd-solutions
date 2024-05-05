d61a3824d6b18232ac67a655333fd565f44ac7ba
Marília Druzian <mariliadruzian@gmail.com>
2024-05-05 19:47:38 -0300

    produto simples

---
+++ beecrowd/1004-produto-simples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Bee1004
+{
+    static void Main(string[] args)
+    {
+        int x, y, produto;
+
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        produto = x * y;
+
+        Console.WriteLine($"PROD = {produto}");
+    }
+}
