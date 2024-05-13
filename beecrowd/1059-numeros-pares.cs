9407b94fd4548f1d673aa9bc1182a249095b091c
Marília Druzian <mariliadruzian@gmail.com>
2024-05-12 23:40:48 -0300

    números pares

---
+++ beecrowd/1059-numeros-pares.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Bee1059
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i <= 100; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
