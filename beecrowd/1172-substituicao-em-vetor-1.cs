6a220530483c8c6b572fc74537fcf46abd3df5cd
Marília Druzian <mariliadruzian@gmail.com>
2024-06-19 22:10:40 -0300

    substituição em vetor 1

---
+++ beecrowd/1172-substituicao-em-vetor-1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Bee1172
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[10];
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+
+            if (vetor[i] <= 0)
+            {
+                vetor[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.WriteLine($"X[{i}] = {vetor[i]}");
+        }
+    }
+}
