226150d1d94624a628f426ee348cbad4344611dd
Marilia Druzian <mariliadruzian@gmail.com>
2024-07-19 22:14:36 -0300

    sequência IJ 1

---
+++ beecrowd/1095-sequencia-ij-1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Bee1095
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        for (int j = 60; j >= 0; j -= 5)
+        {
+            Console.WriteLine($"I={i} J={j}");
+            i += 3;
+        }
+    }
+}
